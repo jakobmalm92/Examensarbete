1d5d3c35e90e12eff6d5c222935dbad3d7c6ef4e
jakobmalm92 <malm92_@hotmail.com>
2025-04-21 11:30:53 +0200

    WIP

--- examensarbete-ai/src/app/components/Footer.tsx
+++ examensarbete-ai/src/app/components/Footer.tsx
@@ -1,4 +1,3 @@
-// components/Footer.tsx
 export default function Footer() {
     return (
       <footer className="text-center text-sm text-gray-600 dark:text-gray-500 py-6 border-t border-gray-200 dark:border-gray-700">
