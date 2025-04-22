ea2a8390dabcabe60f33b213f9b3492e2a886f18
jakobmalm92 <malm92_@hotmail.com>
2025-04-22 15:26:17 +0200

    WIP

--- examensarbete-ai/src/app/login/page.tsx
+++ examensarbete-ai/src/app/login/page.tsx
@@ -22,7 +22,7 @@ export default function LoginPage() {
     }
 
     try {
-      const response = await fetch("http://localhost:5030/api/login", {
+      const response = await fetch("http://localhost:5030/api/auth/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
